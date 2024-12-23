9171422f949ebab2a82820582bbedc5f85bfa1b5
Reine2001 <119446462+Reine2001@users.noreply.github.com>
2024-12-23 16:00:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -175,11 +175,12 @@ def main():
         # Afficher l'image au centre de la sous-colonne du milieu
         with subcol2:
             st.image("icone1.png", caption="LegiChat", width=150)
-            st.markdown("<p style="text-align: center; color: #4CAF50; font-size: 18px;">
-            LegiChat est un chatbot qui fournit des réponses juridiques basées sur les textes en vigueur au Burkina Faso. 
-            Il vous aide à naviguer à travers les lois et décrets, en répondant à vos questions sur des sujets juridiques spécifiques.
-            </p>", unsafe_allow_html=True)
-
+            st.markdown("""
+                <p style="text-align: center; color: #4CAF50; font-size: 18px;">
+                LegiChat est un chatbot qui fournit des réponses juridiques basées sur les textes en vigueur au Burkina Faso. 
+                Il vous aide à naviguer à travers les lois et décrets, en répondant à vos questions sur des sujets juridiques spécifiques.
+                </p>
+                """, unsafe_allow_html=True)
         
 
     # Titre principal sous l'image
